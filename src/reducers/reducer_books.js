4dfba0300b314973e899cdaf6a00f549a2944557
Juan Faricelli <juan.faricelli@outlook.com>
2017-09-08 01:00:39 -0300

    section 4 finished

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Libro 1', pages: 40},
+    { title: 'Libro 2', pages: 100},
+    { title: 'Libro 3', pages: 30},
+    { title: 'Libro 4', pages: 200},
+  ];
+}
